2e9f6dad3a679e388e9cdef1418069dbe1e784a1
dylen400mh <dylenbelanger2004@gmail.com>
2024-09-10 09:52:12 -0400

    Create Home component and delete Portfolio component (not needed)

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,2 +1,26 @@
-const Home = () => {};
+const Home = () => {
+  const handleScroll = (sectionId: string) => {
+    const element = document.getElementById(sectionId);
+    if (element) element.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <section
+      id="home"
+      className="bg-[#1a1a1a] text-white min-h-screen flex flex-col justify-center items-center p-8"
+    >
+      <h1 className="text-4xl font-extrabold mb-4">Hi, I'm Dylen Belanger</h1>
+      <p className="text-lg mb-6 text-gray-300 max-w-xl text-center">
+        3rd Software Engineering student at Western University.
+      </p>
+      <button
+        onClick={() => handleScroll("about")}
+        className="bg-gray-600 text-white px-6 py-3 rounded-lg hover:bg-gray-500 transition-colors duration-300"
+      >
+        Get to know me!
+      </button>
+    </section>
+  );
+};
+
 export default Home;
